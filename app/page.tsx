5545c2a502ff94803c76604c808eed38c22aa4fa
shefayetnayon-dev <shefayetnayon.dev@outlook.com>
2025-09-21 10:44:28 +0600

    new update

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="">
-      <h1>hello world</h1>
+      <h1 className="text-5xl font-bold underline">hello world</h1>
     </div>
   );
 }
