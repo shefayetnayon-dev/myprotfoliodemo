bb707d02b41bc66574d8c729e39f03dc597aaa85
shefayetnayon-dev <shefayetnayon.dev@outlook.com>
2025-09-22 00:52:16 +0600

    third

--- app/components/RightBar.tsx
+++ app/components/RightBar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function RightBar() {
   return (
     <div className='w-[5%] bg-amber-700 rounded p-5'>
-        icon menu
+        icon me0nu
     </div>
   )
 }
