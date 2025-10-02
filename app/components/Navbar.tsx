9233b78a571417a310481f5d5c048f7d35eaa17c
shefayetnayon-dev <shefayetnayon.dev@outlook.com>
2025-10-02 21:42:01 +0600

    mobile load done

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -329,7 +329,7 @@ const Navbar = () => {
             >
               <div className="flex flex-col space-y-2 pt-2">
                 {menuItems.map((item) => (
-                  <a
+                  <Link
                     key={item.name}
                     href={item.href}
                     className="relative px-3 py-2 rounded-md text-base font-medium text-white hover:text-indigo-600 transition-all duration-200 group"
@@ -338,7 +338,7 @@ const Navbar = () => {
                     {item.name}
                     {/* Underline animation for mobile */}
                     <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-indigo-600 transition-all duration-300 group-hover:w-full"></span>
-                  </a>
+                  </Link>
                 ))}
               </div>
               
